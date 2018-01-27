81018e159e5335953eb80e9170a87ba3d909d06e
natehitchcock <tripnull@gmail.com>
2018-01-27 02:03:49 -0800

    moved gamestate

--- src/GameState.ts
+++ src/GameState.ts
@@ -36,3 +36,26 @@ export default class GameState {
         return this.GameObjectById[id];
     }
 }
+
+const Game = new GameState();
+const tickRate = 0.016 * 1000;
+// Add initial objects here
+
+// Start game state
+Game.Start();
+
+let lastFrame = Date.now();
+
+const updateLoop = () => {
+    const thisFrame = Date.now();
+    let dt = thisFrame - lastFrame;
+    // Convert dt to seconds
+    dt /= 1000;
+
+    Game.Update(dt);
+    
+    lastFrame = thisFrame;
+};
+
+window.setInterval(updateLoop, tickRate);
+
--- src/index.tsx
+++ src/index.tsx
@@ -8,25 +8,3 @@ import './vis';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
-
-const Game = new GameState();
-const tickRate = 0.016 * 1000;
-// Add initial objects here
-
-// Start game state
-Game.Start();
-
-let lastFrame = Date.now();
-
-const updateLoop = () => {
-    const thisFrame = Date.now();
-    let dt = thisFrame - lastFrame;
-    // Convert dt to seconds
-    dt /= 1000;
-
-    Game.Update(dt);
-    
-    lastFrame = thisFrame;
-};
-
-window.setInterval(updateLoop, tickRate);
