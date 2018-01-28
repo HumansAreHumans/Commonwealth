e76403c5d22f08ce15c2ccfe0f16f234f3449489
lye <lye@>
2018-01-28 10:33:54 -0800

    Fix paths.
    fuck my life i'm about to commit to aseprite.

--- fix-json-paths.sh
+++ fix-json-paths.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find public/img -iname '*.json' -print0 | xargs -0 sed -i -e 's/C:\\\\Users\\\\lawton\\\\Desktop\\\\raspberry\\\\public\\\\img/./'
+find public/img/ui -iname '*.json' -print0 | xargs -0 sed -i -e 's/C:\\\\Users\\\\lawton\\\\Desktop\\\\raspberry\\\\public\\\\img\\\\ui/./'
