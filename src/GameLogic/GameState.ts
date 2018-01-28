9b8016300e421350a00cc89b510afbf095020949
natehitchcock <tripnull@gmail.com>
2018-01-27 16:48:17 -0800

    added entity add handler

--- src/GameLogic/GameState.ts
+++ src/GameLogic/GameState.ts
@@ -1,15 +1,19 @@
 import GameObject from './GameObject';
 import { GeneratePlanet } from './PlanetGenerator';
 
+type AddedHandler = (entity: GameObject) => void;
+
 export class GameState {
     private GameObjects: Array<GameObject>;
     private GameObjectById: {[key: number]: GameObject};
     private hasStarted: boolean;
+    private addedHandlers: AddedHandler[];
 
     constructor() {
         this.GameObjectById = {};
         this.GameObjects = new Array<GameObject>();
         this.hasStarted = false;
+        this.addedHandlers = [];
     }
 
     Start() {
@@ -38,10 +42,18 @@ export class GameState {
         if (this.hasStarted) {
             go.Start();
         }
+
+        this.addedHandlers.forEach(handler => {
+            handler(go);
+        });
     }
 
     GetGameObjectById(id: number): GameObject {
         return this.GameObjectById[id];
+    }
+
+    onEntityAdded(handler: (entity: GameObject) => void) {
+        this.addedHandlers.push(handler);
     }
 }
 
