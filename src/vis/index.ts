16c9b7c3e75768870dd92def92991b527a32880b
natehitchcock <tripnull@gmail.com>
2018-01-28 13:47:49 -0800

    removed logs

--- src/vis/index.ts
+++ src/vis/index.ts
@@ -107,9 +107,7 @@ const BindEntityEvents = (entity: Planet) => {
         }]);
     });
 
-    console.log('added probe create event');
     entity.On('probeCreated', (data: any) => {
-        console.log('got probe created event');
         networkData.edges.add([{
             from: data.sourcePlanet.id,
             to: data.destinationPlanet.id
